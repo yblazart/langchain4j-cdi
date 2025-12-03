d689f36840495075b89b188367d7baa99fa0513f
Yann Blazart <yann.blazart@bycode.fr>
2025-12-03 19:44:48 +0100

    fix test

--- langchain4j-cdi-core/src/test/java/dev/langchain4j/cdi/plugin/CommonLLMPluginCreatorTest.java
+++ langchain4j-cdi-core/src/test/java/dev/langchain4j/cdi/plugin/CommonLLMPluginCreatorTest.java
@@ -183,6 +183,7 @@ class CommonLLMPluginCreatorTest {
         List<DummyAll.ToInjectAll> toInjectAllList = new ArrayList<>();
         toInjectAllList.add(new DummyAll.ToInjectAllBeanA());
         toInjectAllList.add(new DummyAll.ToInjectAllBeanB());
+        when(DUMMYALL_TOINJECTALL_INSTANCE_MOCKED.isResolvable()).thenReturn(true);
         when(DUMMYALL_TOINJECTALL_INSTANCE_MOCKED.stream()).thenReturn(toInjectAllList.stream());
 
         BeanManager bm = mock(BeanManager.class);
